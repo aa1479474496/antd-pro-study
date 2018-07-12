cd569a0339a0c56bfbbe9a2a68b60294b9f36085
zhashaonan <1479474496@qq.com>
2018-07-12 18:36:21 +0800

    add proxy

--- src/models/list.js
+++ src/models/list.js
@@ -1,6 +1,15 @@
+import { query } from '../services/api'
+
+
 export default {
   namespace: 'list',
   state: {
     citys: ['上海', '北京', '广州']
+  },
+  effects: {
+    *fetch(_, { call, put }) {
+      const response = yield call(query);
+      console.log(response);
+    }
   }
 }
--- src/routes/List.js
+++ src/routes/List.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva';
 
-const List = ({ list }) => {
+// const List = ({ list }) => {
 
-  let { citys } = list;
-  let item = citys.map((i, index) => {
-    return (
-      <h3 key={index}>{i}</h3>
-    );
-  });
+//   let { citys } = list;
+//   let item = citys.map((i, index) => {
+//     return (
+//       <h3 key={index}>{i}</h3>
+//     );
+//   });
 
-  return (
-    <div>{item}</div>
-  );
-}
+//   return (
+//     <div>{item}</div>
+//   );
+// }
 
-export default connect(({ list }) => ({
+// export default connect(({ list }) => ({
+//   list
+// }))(List);
+
+
+@connect(({ list }) => ({
   list
-}))(List);
+}))
+export default class List extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'list/fetch',
+    });
+  }
+
+  render() {
+    const { list } = this.props;
+    // console.log(list);
+    let { citys } = list;
+    let item = citys.map((i, index) => {
+      return (
+        <h3 key={index}>{i}</h3>
+      );
+    });
+
+    return (
+      <div>{item}</div>
+    );
+  }
+}
 
